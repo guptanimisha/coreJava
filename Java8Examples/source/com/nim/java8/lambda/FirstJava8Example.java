9d5d3ba9d61b09cb7f10146d78bd3d08693bbe6a
nimg87 <ng.nimishagupta@gmail.com>
2018-03-15 20:29:41 +0530

    This is the first dummy commit for Java 8 Lambda Expressions example.

---
+++ Java8Examples/source/com/nim/java8/lambda/FirstJava8Example.java
@@ -0,0 +1,13 @@
+package com.nim.java8.lambda;
+/**
+ * This is the first class for Java 8 Lambda Expressions.
+ * I have used FileFilter functional Interface for this example.
+ * Functional Interfaces - Interfaces that have only one abstract interface.
+ * We can use only functional interfaces for lambda expressions.
+ * 
+ * @author Nimisha
+ *
+ */
+public class FirstJava8Example {
+
+}
