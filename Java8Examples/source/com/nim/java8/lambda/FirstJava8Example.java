eb635082eb0ebd8a1a5893a510bb546746fcc1fe
nimg87 <ng.nimishagupta@gmail.com>
2018-03-15 20:41:48 +0530

    Adding a notes files and correct a mistake in comments.

--- Java8Examples/source/com/nim/java8/lambda/FirstJava8Example.java
+++ Java8Examples/source/com/nim/java8/lambda/FirstJava8Example.java
@@ -2,7 +2,7 @@ package com.nim.java8.lambda;
 /**
  * This is the first class for Java 8 Lambda Expressions.
  * I have used FileFilter functional Interface for this example.
- * Functional Interfaces - Interfaces that have only one abstract interface.
+ * Functional Interfaces - Interfaces that have only one abstract method.
  * We can use only functional interfaces for lambda expressions.
  * 
  * @author Nimisha
